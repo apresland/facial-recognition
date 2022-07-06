885b5831213f4a6b6aeabdda3582a10adc2bc840
apresland <andrew.presland@gmail.com>
2022-07-06 13:45:41 +0200

    Adds show image in window in prep for video

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 
 int main()
 {
+    cv::namedWindow("detections", CV_WINDOW_AUTOSIZE);
     cv::Mat frame = cv::imread("input1.jpg");
   
     FaceDetector _detector;
@@ -12,5 +13,7 @@ int main()
         cv::rectangle(frame, r, cv::Scalar(255, 0, 255), 2);
     }
 
-    cv::imwrite("output.jpg", frame);
+    //cv::imwrite("output.jpg", frame);
+    cv::imshow("detections", frame);
+    cv::waitKey(0);
 }
